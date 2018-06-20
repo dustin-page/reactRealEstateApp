2af7cbb7e662b1a42f0c883f01aefd0d0c7dac22
Dustin Page <dustinpage@live.com>
2018-06-20 14:00:01 -0500

    Build some components

---
+++ src/main-page/featured-house.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import HouseDetail from '../shared-components/house-detail';
+
+const FeaturedHouse = (props) => {
+    if (props.house) return (
+        <div>
+            <div className="row featuredHouse">
+                <h3 className="col-md-12 text-center">
+                    Featured House
+                </h3>
+            </div>
+            <HouseDetail house={props.house} />
+        </div>
+    );
+    return (
+        <div>No featured house at this time</div>
+    );
+};
+
+
+
+
+export default FeaturedHouse;
+
+
--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './main-page.css';
 import Header from '../shared-components/header';
+import FeaturedHouse from './featured-house';
+import HouseDetail from '../shared-components/house-detail';
+import HouseFilter from '../shared-components/house-filter';
+import SearchResults from '../search-results';
 
 class App extends Component {
 
@@ -9,6 +13,14 @@ class App extends Component {
     super(props); //Call the base class constructor 'Component' with the 'props' argument
     //State object
     this.state = {}; //Set to empty object or provide initial values for some of the state properties used in this component
+  }
+  //Another way to set state is to use a 'property initializer' like this. Note its not a constructor and doesn't need to call super()
+  //state = {};
+
+  //Good place to call methods that make API requests is on the componentDidMount Lifecycle method of React
+  //Called by React right after a component is mounted
+  componentDidMount() {
+    this.fetchHouses();
   }
 
   //Load the JSON with the fetch API
@@ -21,6 +33,7 @@ class App extends Component {
         //'this' is always a reference to what is directly around the arrow function; in this case the Class 'App' object
 
         this.determineFeaturedHouse();
+        this.determineUniqueCountries();
 
       })
   }
@@ -34,13 +47,47 @@ class App extends Component {
     };
   }
 
+  determineUniqueCountries = () => {
+    const countries = this.allHouses
+      ? Array.from(new Set(this.allHouses.map(h => h.country)))
+      : [];
+    countries.unshift(null);
+    this.setState({ countries });
+  }
+
+  filterHouses = (country) => {
+    this.setState({ activeHouse: null });
+    const filteredHouses = this.allHouses.filter((h) => h.country === country);
+    this.setState({ filteredHouses });
+    this.setState({ country });
+  }
+
+  setActiveHouse = (house) => {
+    this.setState({activeHouse: house});
+  }
+
   render() {
+
+    let activeComponent = null;
+    //Render Components based on if statements
+    if (this.state.country)
+    activeComponent = <SearchResults country={this.state.country} 
+      filteredHouses={this.state.filteredHouses} setActiveHouse={this.setActiveHouse} />;
+    if (this.state.activeHouse)
+      activeComponent = <HouseDetail house={this.state.activeHouse}/>;
+    if (!activeComponent)
+      activeComponent = <FeaturedHouse house={this.state.featuredHouse} />;
+
     return (
       <div className="container">
         <Header subtitle="Providing houses all over the world" /> 
+        <HouseFilter countries={this.state.countries} filterHouses={this.filterHouses}/>
+        {activeComponent}
+        
       </div>
     );
   }
 }
 
 export default App;
+ 
