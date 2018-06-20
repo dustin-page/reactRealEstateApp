ca598b1041dad252e4fe816c7b64d1b77248da82
Dustin Page <dustinpage@live.com>
2018-06-20 15:56:15 -0500

    Fix HouseDetail props

--- src/main-page/featured-house.js
+++ src/main-page/featured-house.js
@@ -9,7 +9,7 @@ const FeaturedHouse = (props) => {
                     Featured House
                 </h3>
             </div>
-            <HouseDetail />
+            <HouseDetail house={props.house}/>
         </div>
     );
     return (
